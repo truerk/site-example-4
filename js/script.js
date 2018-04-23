4f7c17ae54248d858751ddf2b497716748540532
truerk <truerk@yandex.ru>
2018-04-23 11:44:10 +0400

    Инфо панель слайдера отдельно

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 document.addEventListener('DOMContentLoader',function(){
 
 });
+//btn.addEventListener('click', function(){changeBgImg();ещё();ещё();....});
+//block.style.backgroundImage = "url('img/img2.jpg')" 
+/*
+
+var block = document.getElementById('block');
+
+function changeBgImg(){
+    block.style.backgroundImage = "url('https://cs7062.vk.me/c540107/v540107359/2729/fYQlS_23QdA.jpg')";
+}
+
+changeBgImg();
+*/
