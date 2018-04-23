fa56d907c7906e26ac21aadee6b8bab770eaa693
truerk <truerk@yandex.ru>
2018-04-23 17:05:26 +0400

    Открытие инфы фильма

--- js/script.js
+++ js/script.js
@@ -1,6 +1,37 @@
-document.addEventListener('DOMContentLoader',function(){
+document.addEventListener('DOMContentLoaded',function(){
+  //var btn_view = document.getElementsByClassName('movies-info');
+  var btn_view = document.getElementsByClassName('movies');
 
+
+
+  var open_detail = function(){
+    //var parent = this.parentNode.parentNode.id;
+    var parent = this.parentNode.id;
+    var elem = document.getElementById(parent);    
+    var detail = elem.getElementsByClassName('movies-detail');
+    detail[0].classList.add("active-detail");    
+    //this.addEventListener('click', close_detail);  
+   // event.stopPropagation();
+  };
+
+  /*var close_detail = function(){
+    var parent = document.getElementById(this.parentNode.id);
+    //var detail = parent.getElementsByClassName('movies-detail');
+    //detail[0].classList.remove("active-detail");
+    var active = parent.getElementsByClassName('active-detail');
+    active[0].classList.remove('active-detail');
+  }
+*/
+for (var i = 0; i < btn_view.length; i++) {
+    btn_view[i].addEventListener('click', open_detail);
+}
 });
+  
+
+/*document.addEventListener('DOMContentLoaded',function(){
+  var btn_view = document.getElementsByClassName('downloads');
+  btn_view.addEventListener('click', a(), false);  
+});*/
 //btn.addEventListener('click', function(){changeBgImg();ещё();ещё();....});
 //block.style.backgroundImage = "url('img/img2.jpg')" 
 /*
@@ -13,3 +44,4 @@ function changeBgImg(){
 
 changeBgImg();
 */
+//var btn_view = document.getElementsById('iron');
