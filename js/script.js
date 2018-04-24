4af017e83c3939c84a6d4b15ef343805e1deced7
Nickolay Eryomin <truerk@yandex.ru>
2018-04-24 15:19:55 +0400

    Адаптировал

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded',function(){
+
   var movie_info_open = document.getElementsByClassName('movies');
-  var detail_all = document.getElementsByClassName('movies-detail');  
+  var detail_all = document.getElementsByClassName('movies-detail');
+  var burger_button = document.getElementById('burger_button');
+  var content = document.getElementById('content');
+  var burger_button_close = document.getElementById('burger_button_close');
+  var mobile_menu = document.getElementById('mobile_menu');    
 
   var open_detail = function(){
     var parent = this.parentNode.id;
@@ -10,13 +15,11 @@ document.addEventListener('DOMContentLoaded',function(){
       detail_all[i].classList.remove("active-detail");
     }    
     detail[0].classList.add("active-detail");
-    close_detail(parent);    
+    close_detail(detail);    
   };
 
-  var close_detail = function(par){
-    var parent_id = par;
-    var parent = document.getElementById(parent_id);
-    var detail = parent.getElementsByClassName('movies-detail');    
+  var close_detail = function(det){
+    var detail = det;
     detail[0].onclick = function(){
       this.classList.remove("active-detail");
     }
@@ -25,6 +28,22 @@ document.addEventListener('DOMContentLoaded',function(){
   for (var i = 0; i < movie_info_open.length; i++) {
       movie_info_open[i].addEventListener('click', open_detail);
   }
+
+  burger_button.onclick = function(){
+    mobile_menu.style.display = 'flex';
+    burger_button_close.style.display = 'flex';
+    burger_button.style.display = 'none';
+  };
+  burger_button_close.onclick = function(){
+    mobile_menu.style.display = 'none';
+    burger_button_close.style.display = 'none';
+    burger_button.style.display = 'flex';
+  };
+  content.onclick = function(){
+    mobile_menu.style.display = 'none';
+    burger_button_close.style.display = 'none';
+    burger_button.style.display = 'flex';
+  };
   
 });
 //btn.addEventListener('click', function(){changeBgImg();ещё();ещё();....});
